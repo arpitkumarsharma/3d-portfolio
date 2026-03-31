ca9973298a2163619f7c639a858f43bd89b3ea59
arpitkumarsharma <arpitkumarsharmaengg@gmail.com>
2026-03-31 10:45:42 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,10 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I am currently working as subject Matter Expert at arcgate. Experienced Subject matter expert with a demonstrated history of working in the Software Testing and outsourcing/offshoring industry. Skilled in Microsoft Word, Data Research, Online Research, Business Service Management, Internal Audit, Analytical Skills, Service-Level Agreements (SLA),  Management Information Systems (MIS), Customer Relationship Management (CRM), Business Intelligence, Excel Reporting Dashboards and Data Visualization, Team Building and Management, Leadership. Strong professional with a Bachelor of Technology (B.Tech.) focused in Computer Science        </p>
+          I am currently working as subject Matter Expert at arcgate. Experienced Subject matter expert with a demonstrated history of working
+          in the Software Testing and outsourcing/offshoring industry. Skilled in Microsoft Word, Data Research, Online Research, Business Service Management, Internal Audit, Analytical Skills, Service-Level Agreements (SLA),
+          Management Information Systems (MIS), Customer Relationship Management (CRM), Business Intelligence, Excel Reporting Dashboards and Data Visualization, 
+          Team Building and Management, Leadership. Strong professional with a Bachelor of Technology (B.Tech.) focused in Computer Science        </p>
       </div>
     </div>
   );
