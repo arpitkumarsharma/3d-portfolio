a86523231f390a733b9c39fd66722fb865bd11d3
arpitkumarsharma <arpitkumarsharmaengg@gmail.com>
2026-03-31 10:39:53 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,27 +11,24 @@ const Contact = () => {
             <h4>Connect</h4>
             <p>
               <a
-                href="https://www.linkedin.com/in/akashrmalhotra/"
+                href="www.linkedin.com/in/arpit-kumar-sharma-95983a59"
                 target="_blank"
                 rel="noreferrer"
                 data-cursor="disable"
               >
-                LinkedIn — akashrmalhotra
+                LinkedIn — Arpit Kumar Sharma
               </a>
             </p>
             <h4>Education</h4>
-            <p>
-              PGPWE (MBA), Indian Institute of Management, Lucknow — 2021–2023
-            </p>
-            <p>
-              B.Tech Computer Science, Manav Rachna Educational Institutions —
+              <p>
+              B.Tech Computer Science, Rajasthan Technical University —
               2009–2013
             </p>
           </div>
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/akashrmalhotra"
+              href="https://github.com/arpitkumarsharma"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -40,7 +37,7 @@ const Contact = () => {
               GitHub <MdArrowOutward />
             </a>
             <a
-              href="https://www.linkedin.com/in/akashrmalhotra/"
+              href="www.linkedin.com/in/arpit-kumar-sharma-95983a59"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -49,7 +46,7 @@ const Contact = () => {
               LinkedIn <MdArrowOutward />
             </a>
             <a
-              href="https://www.youtube.com/@Leftbraincoder"
+              href="https://www.youtube.com/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -58,7 +55,7 @@ const Contact = () => {
               YouTube <MdArrowOutward />
             </a>
             <a
-              href="https://www.instagram.com/leftbraincoder/"
+              href="https://www.instagram.com/arpitksharma/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -69,7 +66,7 @@ const Contact = () => {
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Akash Malhotra</span>
+              Designed and Developed <br /> by <span>Arpit Kumar Sharma</span>
             </h2>
             <h5>
               <MdCopyright /> 2026
